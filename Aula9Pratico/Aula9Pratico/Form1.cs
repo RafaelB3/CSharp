98a9ddb0b824079af5d1a6f0fd4abe89ef0dd86a
Rafael Brisola <69803482+RafaelB3@users.noreply.github.com>
2021-01-27 14:55:39 -0300

    Add files via upload
    Exercícios em C#,

---
+++ Aula9Pratico/Aula9Pratico/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+
+namespace Aula9Pratico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string aux="";
+            double[] vetor = new double[20];
+                        
+            for (int i = 0; i < 20; i++)
+            {
+                 aux = Interaction.InputBox("entre com um numero", "entre com um numero");
+                if (!(double.TryParse(aux ,out vetor[i])))
+                {
+                         MessageBox.Show("invalido");
+                         i--;
+                }
+                
+            }
+            Array.Reverse(vetor);
+
+            aux = "";
+            for (int i = 0; i < 20; i++)
+            {
+                aux += vetor[i].ToString() + "\n";
+            }
+            MessageBox.Show(aux);
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string aux = "";
+            double[] vetor = new double[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+                aux = Interaction.InputBox("entre com um numero", "entre com um numero");
+                if (!(double.TryParse(aux, out vetor[i])))
+                {
+                    MessageBox.Show("invalido");
+                    i--;
+                }
+
+            }
+            Array.Reverse(vetor);
+
+            aux = "";
+            for (int i = 0; i < 20; i++)
+            {
+                aux += vetor[i].ToString() + "\n";
+            }
+            MessageBox.Show(aux);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int [] Quantidade = new int[10];
+            double [] Preço = new double[10];
+            string aux = ""; 
+
+            for (int i = 0; i < 10; i++)
+            {
+               Interaction.InputBox("Entre com o Valor","Entre com o valor");
+               double.TryParse(aux,out ); 
+            }
+        }
+    }
+}
